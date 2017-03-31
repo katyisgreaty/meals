3461efda01fcc185ad3889d5d6d475c57cae8260
Katy Daviscourt <katy.daviscourt@gmail.com>
2017-03-31 08:41:13 -0700

    write basic code for meal.model.ts

---
+++ app/meal.model.ts
@@ -0,0 +1,14 @@
+export class Meal {
+  public name: string;
+  public details: string;
+  public calories: number;
+  constructor(public name: string, public details: string, public calories: number) { };
+
+  static listOfMeals: Meal[] = [
+    new Meal('Turkey Sandwich', 'Turkey, wheat bread, mayo tomato and lettuce', 400),
+    new Meal('Cereal', 'Lucky Charms and 1% Milk', 310),
+    new Meal('Pasta', 'Rotini and Marinara Sauce', 400),
+  ];
+
+
+}
